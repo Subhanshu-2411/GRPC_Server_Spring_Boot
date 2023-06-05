571f28f11325147b679887dee41e9ac6787d6c8b
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-06-05 21:30:02 +0530

    Added Modules

---
+++ greeting-common/src/test/java/com/example/greetingcommon/GreetingCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
